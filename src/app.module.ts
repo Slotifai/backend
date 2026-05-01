da4b9603f1b0ef056b3a3e2c315d28aac1dd4ce7
stas-dubina <stas.dubina@gmail.com>
2026-05-01 21:59:35 +0300

    feat: configure app module, add helmet and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,50 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Service } from './common/entities/service.entity';
-import { Client } from './common/entities/client.entity';
-import { Appointment } from './common/entities/appointment.entity';
-import { Master } from './common/entities/master.entity';
-import { WorkingHours } from './common/entities/working-hours.entity';
-import { AppointmentNote } from './common/entities/appointment-note.entity';
-import { Review } from './common/entities/review.entity';
-import { User } from './common/entities/user.entity';
-import { AuthModule } from './auth/auth.module';
-import { MastersModule } from './masters/masters.module';
-import { ClientsModule } from './clients/clients.module';
-import { AppointmentsModule } from './appointments/appointments.module';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {ThrottlerModule} from '@nestjs/throttler';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Service} from './common/entities/service.entity';
+import {Client} from './common/entities/client.entity';
+import {Appointment} from './common/entities/appointment.entity';
+import {Master} from './common/entities/master.entity';
+import {WorkingHours} from './common/entities/working-hours.entity';
+import {AppointmentNote} from './common/entities/appointment-note.entity';
+import {Review} from './common/entities/review.entity';
+import {User} from './common/entities/user.entity';
+import {AuthModule} from './auth/auth.module';
+import {MastersModule} from './masters/masters.module';
+import {ClientsModule} from './clients/clients.module';
+import {AppointmentsModule} from './appointments/appointments.module';
+import {ServicesModule} from './services/services.module';
+import {ReviewsModule} from './reviews/reviews.module';
+import {AdminModule} from './admin/admin.module';
+import {ScheduleModule} from '@nestjs/schedule';
+import {NotificationsModule} from './notifications/notifications.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Service, Client, Appointment, Master, WorkingHours, AppointmentNote, Review, User],
-      synchronize: true,
-    }),
-    AuthModule,
-    MastersModule,
-    ClientsModule,
-    AppointmentsModule,
-  ],
+    imports: [
+        ConfigModule.forRoot({isGlobal: true}),
+        ThrottlerModule.forRoot([{ttl: 60000, limit: 10}]),
+        ScheduleModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [Service, Client, Appointment, Master, WorkingHours, AppointmentNote, Review, User],
+            migrations: ['dist/migrations/*.js'],
+            synchronize: false,
+        }),
+        AuthModule,
+        MastersModule,
+        ClientsModule,
+        AppointmentsModule,
+        ServicesModule,
+        ReviewsModule,
+        AdminModule,
+        NotificationsModule,
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {ValidationPipe} from '@nestjs/common';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import helmet from 'helmet';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+    const app = await NestFactory.create(AppModule);
+    app.use(helmet());
+    app.useGlobalPipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true}));
 
-  const config = new DocumentBuilder()
-    .setTitle('Slotifai API')
-    .setDescription('Booking & appointment backend')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  SwaggerModule.setup('api/docs', app, SwaggerModule.createDocument(app, config));
+    const config = new DocumentBuilder()
+        .setTitle('Slotifai API')
+        .setDescription('Booking & appointment backend')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    SwaggerModule.setup('api/docs', app, SwaggerModule.createDocument(app, config));
 
-  await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
